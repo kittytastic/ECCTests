34014c0928868a13b53498d79c8505cfeb86d897
Jack Bird <jack.bird@durham.ac.uk>
2019-01-29 18:49:52 +0000

    Added functions to send errors and updates

--- notifier.py
+++ notifier.py
@@ -1,6 +1,8 @@
 import smtplib
 import config
 
+errorLog = []
+
 def send_email(subject, msg):
 	try:
 		server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
@@ -13,3 +15,19 @@ def send_email(subject, msg):
 		server.quit()
 	except Exception as e:
 		print("Message Failed to send")
+
+def sendUpdate(currentTestCase):
+	send_email("Update: " + currentTestCase, 
+		"Testing is still running and is on " + currentTestCase + ".")
+
+## Instead of sending every error one at a time send them in bulk
+## so you don't get a steady flow of 100s of emails when something goes wrong.
+def addError(errorInfo):
+	errorLog.append(errorInfo)
+
+def sendErrors():
+	message = "\n\n\n".join(errorLog)
+	send_email("Errors: " + len(errorLog), message)
+
+def sendError(errorInfo):
+	send_email("Error", errorInfo)
