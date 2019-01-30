c35a2bdae0cb473938c4d4a9927a63bcbcdc297e
= <jack@theslingsbys.com>
2019-01-30 17:41:05 +0000

    Added assert error messages. Added message encode then decode test. Added questions sheet message encode and decode examples to hardcoded

--- advanced_test.py
+++ advanced_test.py
@@ -4,6 +4,7 @@ import random
 from answers import repetitionEncoder
 from answers import repetitionDecoder
 from answers import message
+from answers import dataFromMessage
 
 
 def random_noise(m, bits_to_flip):
@@ -34,6 +35,7 @@ def flip_bit(bit):
     else:
         sys.exit("Flipping bit that isn't 0 or 1")
 
+
 def random_message(length):
     m = []
     for i in range(length):
@@ -41,33 +43,42 @@ def random_message(length):
     return m
 
 
-
 def hard_coded_tests():
-    assert repetitionEncoder([0], 4) == [0, 0, 0, 0]
-    assert repetitionEncoder([1], 5) == [1, 1, 1, 1, 1]
+    m = "Failed to encode to repetition code"
+    assert repetitionEncoder([0], 4) == [0, 0, 0, 0], m
+    assert repetitionEncoder([1], 5) == [1, 1, 1, 1, 1], m
 
-    assert repetitionDecoder([1, 1, 1, 1]) == [1]
-    assert repetitionDecoder([1, 1, 1, 1, 1]) == [1]
-    assert repetitionDecoder([1, 1, 0, 0]) == []
-    assert repetitionDecoder([1, 1, 0, 0, 0]) == [0]
-    assert repetitionDecoder([0, 0, 0, 0]) == [0]
-    assert repetitionDecoder([1, 0, 0, 0]) == [0]
-    assert repetitionDecoder([1, 1, 1, 0]) == [1]
+    m = "Failed to decode repetition code"
+    assert repetitionDecoder([1, 1, 1, 1]) == [1], m
+    assert repetitionDecoder([1, 1, 1, 1, 1]) == [1], m
+    assert repetitionDecoder([1, 1, 0, 0]) == [], m
+    assert repetitionDecoder([1, 1, 0, 0, 0]) == [0], m
+    assert repetitionDecoder([0, 0, 0, 0]) == [0], m
+    assert repetitionDecoder([1, 0, 0, 0]) == [0], m
+    assert repetitionDecoder([1, 1, 1, 0]) == [1], m
 
-    assert message([1]) == [0, 0, 1, 1]
-    assert message([0, 0, 1]) == [0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0]
-    assert message([0, 1, 1, 0]) == [0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0]
-    assert message([1, 1, 1, 1, 0, 1]) == [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0]
-    assert message([0, 1, 1, 0, 1]) == [0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0]
+    # All from questions
+    m = "Failed correctly encode message to data"
+    assert message([1]) == [0, 0, 1, 1], m
+    assert message([0, 0, 1]) == [0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0], m
+    assert message([0, 1, 1, 0]) == [0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0], m
+    assert message([1, 1, 1, 1, 0, 1]) == [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0], m
+    assert message([0, 1, 1, 0, 1]) == [0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0], m
+
+    # All from questions
+    assert dataFromMessage([1, 0, 0, 1, 0, 1, 1, 0, 1, 0]) == [], "Failed to spot error when getting data from message when message is too short"
+    assert dataFromMessage([1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0]) == [], "Failed to spot error when getting data from message when l>number of bits in message"
+    assert dataFromMessage([0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0]) == [0, 1, 1, 0, 1], "Failed to get correct data from message"
 
 
 # Encode a message using repetition code then decodes it
+# Checks: repetitionEncoder(), repetitionDecoder()
 def test_rep_encode_decode():
     original_mes = [random.randint(0, 1)]
     encoded = repetitionEncoder(original_mes, random.randint(2, 20))
     decoded = repetitionDecoder(encoded)
 
-    assert original_mes == decoded
+    assert original_mes == decoded, "Repetition code failed to get encoded then immediately decoded"
 
 
 # Encode a message using repetition code applies random noise then decodes it
@@ -84,10 +95,10 @@ def test_rep_encode_noise_decode():
         if error_bits == code_len // 2:
             expected = []
 
-    assert decoded == expected
+    assert decoded == expected, "Repetition code failed to decode correctly"
 
 
-# Check messages are of an expected length
+# Check message() returns messages of correct length
 def test_message_length():
     # max message length 100
     original = random_message(random.randint(1,100))
@@ -99,7 +110,17 @@ def test_message_length():
         r += 1
         l = 2 ** r - r - 1
 
-    assert l == length
+    assert l == length, "Message failed to encode to an expected length"
+
+
+# Encodes a message then immediately decode it
+# Checks basic functionality for: message(), dataFromMessage()
+def test_message_encode_decode():
+    original = random_message(random.randint(1,100))
+    mes = message(original)
+    decode = dataFromMessage(mes)
+
+    assert decode == original, "Message failed to get encoded and decoded (no hamming codes) without changing"
 
 
 def test():
@@ -109,18 +130,18 @@ def test():
     print("    - Passed")
 
     # Test repetition code
-    print("Trying auto generated repetition code tests")
+    print("Trying repetition code tests")
     for i in range(10000):
         test_rep_encode_decode()
         test_rep_encode_noise_decode()
     print("    - Passed")
 
-    print("Trying auto generated message only tests")
+    print("Trying message only tests")
     for i in range(10000):
         test_message_length()
+        test_message_encode_decode()
 
     print("    - Passed")
-
 
     print("All tests passed")
 
